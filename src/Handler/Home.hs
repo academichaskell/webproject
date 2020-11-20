f512cefa3c9bee67e61245aa0778cf1672ef95b5
Lucas Davi <academichaskell@gmail.com>
2020-11-20 15:26:57 -0300

    Creating homepage structure.

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -3,10 +3,13 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
 module Handler.Home where
 
 import Import
 import Database.Persist.Postgresql
 
 getHomeR :: Handler Html
-getHomeR = undefined
+getHomeR = do
+    defaultLayout $ do
+        $(whamletFile "templates/homepage.hamlet")
