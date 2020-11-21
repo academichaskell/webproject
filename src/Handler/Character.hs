b639f9135cf68cf4c7fb37872e65fbb8158d845e
Lucas Davi <academichaskell@gmail.com>
2020-11-21 09:21:39 -0300

    Updating a character in database.

--- src/Handler/Character.hs
+++ src/Handler/Character.hs
@@ -10,37 +10,40 @@ import Import
 import Text.Lucius
 --import Database.Persist.Postgresql
 
-formCharacter :: Form Character
-formCharacter = renderDivs $ Character
+formCharacter :: Maybe Character -> Form Character
+formCharacter character = renderDivs $ Character
     <$> areq textField (FieldSettings "Nome do personagem"
                                       Nothing
                                       Nothing
                                       Nothing
                                       [("class", "text-field")]
-                       ) Nothing
+                       ) (fmap characterName character)
     <*> areq textField (FieldSettings "Classe"
                                       Nothing
                                       Nothing
                                       Nothing
                                       [("class", "text-field")]
-                       ) Nothing
+                       ) (fmap characterClass character)
     <*> areq textField (FieldSettings "Arma primária"
                                       Nothing
                                       Nothing
                                       Nothing
                                       [("class", "text-field")]
-                       ) Nothing
+                       ) (fmap characterWeapon character)
 
-getCharacterR :: Handler Html
-getCharacterR = do
-    (widget,_) <- generateFormPost formCharacter
+auxCharacterR :: Route App -> Maybe Character -> Handler Html
+auxCharacterR rt character = do
+    (widget,_) <- generateFormPost (formCharacter character)
     defaultLayout $ do
         toWidgetHead $(luciusFile "templates/avatarRegister.lucius")
         $(whamletFile "templates/avatarRegister.hamlet")
 
+getCharacterR :: Handler Html
+getCharacterR = auxCharacterR CharacterR Nothing
+
 postCharacterR :: Handler Html
 postCharacterR = do
-    ((resp,_),_) <- runFormPost formCharacter
+    ((resp,_),_) <- runFormPost (formCharacter Nothing)
     case resp of
         FormSuccess character -> do
             cid <- runDB $ insert character
@@ -53,3 +56,17 @@ getListCharactersR = do
     defaultLayout $ do
         toWidgetHead $(luciusFile "templates/listCharacters.lucius")
         $(whamletFile "templates/listCharacters.hamlet")
+
+getUpdateCharacterR :: CharacterId -> Handler Html
+getUpdateCharacterR cid = do
+    old <- runDB $ get404 cid
+    auxCharacterR (UpdateCharacterR cid) (Just old)
+
+postUpdateCharacterR :: CharacterId -> Handler Html
+postUpdateCharacterR cid = do
+    ((resp,_),_) <- runFormPost (formCharacter Nothing)
+    case resp of
+        FormSuccess new -> do
+            runDB $ replace cid new
+            redirect (ListCharactersR)
+        _ -> redirect HomeR
