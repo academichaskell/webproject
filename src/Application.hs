0f8a6214204492a61eec56bc787f559c757ea25b
Lucas Davi <academichaskell@gmail.com>
2020-11-22 11:30:04 -0300

    Creating and Updating a monster in database.

--- src/Application.hs
+++ src/Application.hs
@@ -41,6 +41,7 @@ import Handler.Home
 import Handler.Character
 import Handler.User
 import Handler.Session
+import Handler.Monster
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
---
+++ src/Handler/Monster.hs
@@ -0,0 +1,64 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
+module Handler.Monster where
+
+import Import
+import Text.Lucius
+--import Database.Persist.Postgresql
+
+formMonster :: Maybe Monster -> Form Monster
+formMonster monster = renderDivs $ Monster
+    <$> areq textField (FieldSettings "Nome do monstro"
+                                      Nothing
+                                      Nothing
+                                      Nothing
+                                      [("class", "text-field")]
+                       ) (fmap monsterName monster)
+    <*> areq doubleField (FieldSettings "HP"
+                                      Nothing
+                                      Nothing
+                                      Nothing
+                                      [("class", "text-field")]
+                       ) (fmap monsterLife monster)
+
+auxMonsterR :: Route App -> Maybe Monster -> Handler Html
+auxMonsterR rt monster = do
+    (widget,_) <- generateFormPost (formMonster monster)
+    defaultLayout $ do
+        if (rt == MonsterR) then do
+            toWidgetHead $(luciusFile "templates/monsterRegister.lucius")
+            $(whamletFile "templates/monsterRegister.hamlet")
+        else do
+            toWidgetHead $(luciusFile "templates/monsterUpdate.lucius")
+            $(whamletFile "templates/monsterUpdate.hamlet")
+
+getMonsterR :: Handler Html
+getMonsterR = auxMonsterR MonsterR Nothing
+
+postMonsterR :: Handler Html
+postMonsterR = do
+    ((resp,_),_) <- runFormPost (formMonster Nothing)
+    case resp of
+        FormSuccess monster -> do
+            mid <- runDB $ insert monster
+            redirect HomeR --ListMonstersR
+        _ -> redirect HomeR
+
+getUpdateMonsterR :: MonsterId -> Handler Html
+getUpdateMonsterR mid = do
+    old <- runDB $ get404 mid
+    auxMonsterR (UpdateMonsterR mid) (Just old)
+
+postUpdateMonsterR :: MonsterId -> Handler Html
+postUpdateMonsterR mid = do
+    ((resp,_),_) <- runFormPost (formMonster Nothing)
+    case resp of
+        FormSuccess new -> do
+            runDB $ replace mid new
+            redirect (HomeR)  --ListMonstersR
+        _ -> redirect HomeR
+    
